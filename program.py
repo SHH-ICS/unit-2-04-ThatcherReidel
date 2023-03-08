483567db05ad8010faf0e847b2f953a58fd79382
ThatcherReidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-03-08 20:00:06 +0000

    got it working

--- program.py
+++ program.py
@@ -1,19 +1,20 @@
 # My Python Program
-# Task:  use if statements to output the result of the game fizzbuzz.  
+# Task:  use if statements to output the result of the game fizzbuzz.
 # Start at 1
 # For multiples of 3, output   Fizz
 # For multiples of 5, output   Buzz
 # For multiples of 15, output   FizzBuzz
 # End at 32
 
-for fizzbuzz in range(32):
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("fizzbuzz")
-        continue
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
-        continue
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
-        continue
-    print(fizzbuzz)
+for number in range(1, 33):
+    found = False
+    if number % 3 == 0:
+        found = True
+        print("Fizz", end = "")
+    if number % 5 == 0:
+        found = True
+        print("Buzz", end = "")
+    if found == False:
+        print("%d" % (number))
+    else:
+        print("")
